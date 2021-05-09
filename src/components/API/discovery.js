140ae6181baeaa4636ac4d1b18182b854091c9bb
GodLuckMister <paet14@gmail.com>
2021-05-09 14:22:53 +0300

    create class for api

--- src/components/API/discovery.js
+++ src/components/API/discovery.js
@@ -1,10 +1,19 @@
 fetch(
-  'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=y0yYLqXgtKWQACxMls2Nd9iuExOsMHTV',
+  'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&page=2&apikey=y0yYLqXgtKWQACxMls2Nd9iuExOsMHTV',
 )
   .then(response => response.json())
-  .then(r => console.log(r));
-fetch(
-  'https://app.ticketmaster.com/discovery/v2/events/vvG1VZG5LseKSl/images.json?countryCode=US&apikey=y0yYLqXgtKWQACxMls2Nd9iuExOsMHTV',
-)
-  .then(response => response.json())
-  .then(r => console.log(r));
+  .then(r => console.log(r._embedded.events));
+
+class ApiService {
+  constructor() {
+    this.key = 'y0yYLqXgtKWQACxMls2Nd9iuExOsMHTV';
+    this.page = 0;
+    this.searchQuery = '';
+  }
+  async fetchApiServiceAll() {
+    const url = `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&page=${this.page}&apikey=${this.key}`;
+    return await fetch(url)
+      .then(response => response.json())
+      .then(r => r);
+  }
+}
