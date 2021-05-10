d6b63e477827de653d2ac61fc7f4e0d65321083a
GodLuckMister <paet14@gmail.com>
2021-05-11 00:15:34 +0300

    update API Class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './styles.css';
 import './sass/base/fonts.css';
 import './sass/main.scss';
-import './components/index';
+import './js/main';
--- src/js/components/API/discoveryAPI.js
+++ src/js/components/API/discoveryAPI.js
@@ -1,23 +1,18 @@
-// fetch(
-//   'https://app.ticketmaster.com/discovery/v2/events.json?keyword=&countryCode=GB&page=2&apikey=y0yYLqXgtKWQACxMls2Nd9iuExOsMHTV',
-// )
-//   .then(response => response.json())
-//   .then(r => console.log(r._embedded.events))
-//   .catch(error => console.log('hello', error));
-
 export default class ApiService {
   constructor(keyword = '', country = 'US') {
     this.key = 'y0yYLqXgtKWQACxMls2Nd9iuExOsMHTV';
-    this.page = 0;
+    this.url = 'https://app.ticketmaster.com/discovery/v2/events.json?';
+    this.page = 1;
     this.country = country;
     this.keyword = keyword;
   }
   async fetchApiServiceAll() {
-    const url = `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${this.keyword}&countryCode=${this.country}&page=${this.page}&apikey=${this.key}`;
+    const url = `${this.url}keyword=${this.keyword}&countryCode=${this.country}&page=${this.page}&apikey=${this.key}`;
     return await fetch(url)
       .then(response => response.json())
       .catch(error => console.error(error));
   }
+
   incrementPage() {
     this.page += 1;
   }
