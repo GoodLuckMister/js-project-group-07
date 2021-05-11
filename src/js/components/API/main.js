d65901e55b574eab007c08423095167cc603e399
Fomenko Vladyslav <fomenkovlad93@gmail.com>
2021-05-12 02:35:13 +0300

    add makePadinationList function + click function
    убрал return , добавил функцию для пагинации, фетч запихнул в фунцию

--- src/js/components/API/main.js
+++ src/js/components/API/main.js
@@ -1,16 +1,22 @@
 import api from './discoveryAPI';
 import cards from '../renderCard/renderCard';
 import refs from '../refs';
+import makePaginationList from '../pagination/pagination'
 
 refs.form.addEventListener('submit', onSearch);
 const event = new api('Concert', 'US');
-event
+//! вынес fetch в функцию, что бы вызвать ее при клике на номер страницы
+function fetchEvents(event) {
+  event
   .fetchApiServiceAll()
   .then(r => {
     console.log(r._embedded.events);
-    return cards(r._embedded.events, refs.cardsList);
+    //! убрал return, он здесь ничего не делает, а мне нужно прописать пагинацию после рендера
+    cards(r._embedded.events, refs.cardsList);
+    makePaginationList(r, event)
   })
   .catch(e => console.log('hello', e));
+}
 
 function onSearch(e) {
   e.preventDefault();
@@ -18,6 +24,14 @@ function onSearch(e) {
   event.query = e.target.elements.search.value;
   event.location = e.target.elements.country.value;
 }
+
+fetchEvents(event)
+
+refs.paginationList.addEventListener('click', (e) => {
+  
+  event.page = Number(e.target.textContent) - 1
+  fetchEvents(event)
+})
 
 // const bodyRef = document.querySelector('body');
 // bodyRef.addEventListener('keydown', e => {
