d2aa3d9854ea72e48bbe3338b3cbcd4affdaac9a
Elena Vlasenko <olefelena@gmail.com>
2021-05-11 17:26:42 +0300

    логика строки поиска и выбора страны

--- src/js/components/API/discoveryAPI.js
+++ src/js/components/API/discoveryAPI.js
@@ -1,13 +1,14 @@
+const BASIC_URL = 'https://app.ticketmaster.com/discovery/v2/';
+const KEY = 'y0yYLqXgtKWQACxMls2Nd9iuExOsMHTV'
+
 export default class ApiService {
   constructor(keyword = '', country = 'US') {
-    this.key = 'y0yYLqXgtKWQACxMls2Nd9iuExOsMHTV';
-    this.url = 'https://app.ticketmaster.com/discovery/v2/events.json?';
     this.page = 1;
     this.country = country;
     this.keyword = keyword;
   }
   async fetchApiServiceAll() {
-    const url = `${this.url}keyword=${this.keyword}&countryCode=${this.country}&page=${this.page}&apikey=${this.key}`;
+    const url = `${BASIC_URL}events.json?keyword=${this.keyword}&countryCode=${this.country}&page=${this.page}&apikey=${KEY}`;
     return await fetch(url)
       .then(response => response.json())
       .catch(error => console.error(error));
@@ -20,6 +21,19 @@ export default class ApiService {
     this.page -= 1;
   }
   resetPage() {
-    return (this.page = 0);
+    return (this.page = 1);
+  }
+  get query() {
+    return this.keyword;
+  }
+  set query(newKeyword) {
+    this.keyword = newKeyword;
+    resetPage();
+  }
+  get location() {
+    return this.country;
+  }
+  set location(newCountry) {
+    this.country = newCountry;
   }
 }
--- src/js/components/API/main.js
+++ src/js/components/API/main.js
@@ -1,14 +1,26 @@
 import api from './discoveryAPI';
 import cards from '../renderCard/renderCard';
-const cardsList = document.querySelector('.events-section__list');
-let event = new api('Concert', 'US');
-event
+import refs from '../refs'
+
+refs.form.addEventListener('submit', onSearch);
+const event = new api('Concert', 'US');
+
+function onSearch(e) {
+  e.preventDefault();
+
+  event.query = e.target.elements.search.value;
+    event.location = e.target.elements.country.value;
+
+  event
   .fetchApiServiceAll()
   .then(r => {
     console.log(r._embedded.events);
-    return cards(r._embedded.events, cardsList);
+    return cards(r._embedded.events, refs.cardsList);
   })
   .catch(e => console.log('hello', e));
+}
+
+
 
 // const bodyRef = document.querySelector('body');
 // bodyRef.addEventListener('keydown', e => {
