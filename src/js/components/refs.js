2286c0774286392369e2a973b510ed8b7d8074a2
Artem-Rudko <72283896+Artem-Rudko@users.noreply.github.com>
2021-05-15 19:56:21 +0300

    add modal.js files

--- src/js/components/refs.js
+++ src/js/components/refs.js
@@ -3,6 +3,7 @@ export default {
     form: document.querySelector('.form'),
     paginationList: document.querySelector('.pagination__list'),
     header: document.querySelector('.header'),
+    modalEl: document.querySelector('.js-modal'),
     modalCloseBtn: document.querySelector('button[data-action="close-modal"]'),
     modalOverlay: document.querySelector('.modal__overlay'),
     showMoreBtn: document.querySelector('button[data-action="show-more"]')
