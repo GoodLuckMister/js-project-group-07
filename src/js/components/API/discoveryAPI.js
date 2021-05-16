cf264d84c32549bb950bcdfdcbc0522089ff4f85
Elena Vlasenko <olefelena@gmail.com>
2021-05-16 13:53:06 +0300

    add notification in case incorrect search request
    Реализовано через добавление еще одного метода в api, иначе либо ошибка отлавливалась сразу и уведомление уже не появлялось, или уведомление появлялось еще до запроса при стартовом рендере страницы.
    в package-lock.json много изменений после установления pnotify, но они же появляются автоматически

--- src/js/components/API/discoveryAPI.js
+++ src/js/components/API/discoveryAPI.js
@@ -13,12 +13,16 @@ export default class {
       .then(response => response.json())
       .catch(error => console.error(error));
   }
-  //   async fetchDetails(eventID) {
-  //   const url = `${BASIC_URL}events/${eventID}?apikey=${KEY}`;
-  //   return await fetch(url)
-  //     .then(response => response.json())
-  //     .catch(error => console.error(error));
-  // }
+  async fetchDetails(eventID) {
+    const url = `${BASIC_URL}events/${eventID}.json?apikey=${KEY}`;
+    return await fetch(url)
+      .then(response => response.json())
+      .catch(error => console.error(error));
+  }
+  async fetchApiBySearch() {
+       const url = `${BASIC_URL}events.json?keyword=${this.keyword}&countryCode=${this.country}&page=${this.page}&apikey=${KEY}`;
+    return await fetch(url).then(response => response.json());
+  }
   incrementPage() {
     this.page += 1;
   }
