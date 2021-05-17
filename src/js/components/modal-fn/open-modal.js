16f5d9d8279c7d86e43902f1ffe7ef76ff869e9a
Artem-Rudko <72283896+Artem-Rudko@users.noreply.github.com>
2021-05-17 03:36:29 +0300

    add escBtnClose for modal

--- src/js/components/modal-fn/open-modal.js
+++ src/js/components/modal-fn/open-modal.js
@@ -1,6 +1,7 @@
 import refs from '../refs';
 import renderModal from '../modal-fn/modalMarkup';
-import onEscKeyPress from '../modal-fn/close-modal';
+// import onEscKeyPress from '../modal-fn/close-modal';
+// import onModalClose from '../modal-fn/close-modal';
 import { fetchId } from '../API/main';
 import { event } from '../API/main';
 
@@ -14,16 +15,39 @@ export default function onEventClick(event) {
     // const eventDataId = fetchId(eventId);
     // fetchId(eventId).then(eventDataId => renderModal(eventDataId)).cath(e => console.log('error', e));
     fetchId(eventId);
-    console.log(eventId);
+    // console.log(eventId);
     // console.log(eventDataId);
-
-
+    
+    
     // event.fetchDetails(eventId).then(r => console.log(r));
     // const eventDataId = fetchId(eventId);
     // renderModal(eventDataId);
     
     window.addEventListener('keydown', onEscKeyPress);
     
+    refs.modalCloseBtn.addEventListener('click', onModalClose);
+    
+    console.log(refs.modalCloseBtn);
     // console.log(event.target.id);
    
 }
+
+
+function onModalClose(event) {
+    window.removeEventListener('keydown', onEscKeyPress);
+    refs.modalEl.classList.remove('is-open');
+    console.log('click');
+};
+
+function onEscKeyPress(event) {
+    if (event.code === 'Escape') {
+        onModalClose(event)
+    }
+};
+
+
+// refs.modalOverlay.addEventListener('click', (event) => {
+//     if (event.currentTarget === event.target) {
+//         onModalClose()
+//     }
+// });
