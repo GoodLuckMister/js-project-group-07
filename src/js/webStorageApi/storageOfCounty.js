0040bed4db9321c6cb65b1f1d8c49b3f1584b9c2
GodLuckMister <paet14@gmail.com>
2021-05-20 13:17:14 +0300

    upgrade

--- src/js/webStorageApi/storageOfCounty.js
+++ src/js/webStorageApi/storageOfCounty.js
@@ -1,6 +1,3 @@
-// import refs from '../components/refs/refs';
-// import { event } from '../components/search-form/search-form-logic';
-
 const STORAGE_COUNTRY_KEY = 'chosenCountry';
 
 export function saveCountry(country) {
