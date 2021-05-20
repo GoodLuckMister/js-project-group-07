0323af6b0eb8fab3cc5d10fe8881fb94e6ed0f77
GodLuckMister <paet14@gmail.com>
2021-05-20 13:23:50 +0300

    upgrade

--- src/js/components/search-form/search-form-logic.js
+++ src/js/components/search-form/search-form-logic.js
@@ -2,8 +2,11 @@ import refs from '../refs/refs';
 import cards from '../renderCard/renderCard';
 import showNotification from '../pnotifyNotifications/notification';
 import makePaginationList from '../pagination/pagination';
-// import { saveSearchRequest } from '../../webStorageApi/storageOfSearchQuery';
-// import { savedCountry } from '../../webStorageApi/storageOfCounty';
+import { saveSearchRequest } from '../../webStorageApi/storageOfSearchQuery';
+import {
+  savedCountry,
+  clearSavedCountry,
+} from '../../webStorageApi/storageOfCounty';
 import Preloader from '../preloader/Preloader';
 import API from '../../API/DiscoveryAPI';
 
@@ -13,6 +16,7 @@ const preloader = new Preloader();
 
 export default function onSearch(e) {
   event.query = e.target.value.trim();
+  event.location = savedCountry || event.location;
 
   if (event.query === '') {
     return;
@@ -26,6 +30,7 @@ export default function onSearch(e) {
       checkFetch(r, e);
       cards(r._embedded.events, refs.cardsList);
       makePaginationList(r, event);
+      saveSearchRequest();
     })
     .catch(e => console.log('hello', e))
     .finally(() => preloader.remove());
@@ -36,6 +41,38 @@ function checkFetch(r, e) {
     preloader.remove();
     event.query = '';
     e.target.value = '';
+    clearSavedCountry();
     return showNotification();
   }
 }
+
+// import refs from '../refs';
+// import cards from '../renderCard/renderCard';
+// import { event } from '../API/main';
+// import showNotification from '../pnotifyNotifications/notification';
+// import makePaginationList from '../pagination/pagination';
+// import { saveSearchRequest } from '../../webStorageApi/storageOfSearchQuery';
+// import { savedCountry, clearSavedCountry } from '../../webStorageApi/storageOfCounty';
+// import Preloader from '../preloader/Preloader';
+
+// const preloader = new Preloader(refs.containerPreload);
+
+// export default function onSearch(e) {
+//   event.query = e.target.value.trim();
+//   event.location = savedCountry || event.location;
+//   preloader.show();
+
+//   event
+//     .fetchApiBySearch()
+//     .then(r => {
+//       if (!r._embedded) {
+//         clearSavedCountry();
+//         return showNotification();
+//       }
+//       cards(r._embedded.events, refs.cardsList);
+//       makePaginationList(r, event);
+//       saveSearchRequest();
+//     })
+//     .catch(e => console.log('hello', e))
+//     .finally(() => { preloader.remove(); });
+// }
